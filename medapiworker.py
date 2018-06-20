54c3334b876951a28d2fd1728c3fc25afeaebd92
Kacper Szczepańczyk <kacper.szczepanczyk@gmail.com>
2018-06-20 19:57:57 +0200

    details

--- medapiworker.py
+++ medapiworker.py
@@ -5,16 +5,16 @@ cache = IronCache()
 
 # Put an item
 for i in range(0, 5):
-    cache.put(cache="test_cache", key="mykey", value="Hello IronCache!rtyyyyyyyyyyyyyyyyywwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
+    cache.put(cache="test_cache", key="mykey" + str(i), value="Cache" + str(i))
 
 # Get an item
-item = cache.get(cache="test_cache", key="mykey")
+item = cache.get(cache="test_cache", key="mykey3")
 print (item.value)
 
 # Delete an item
 #cache.delete(cache="test_cache", key="mykey")
 
 # Increment an item in the cache
-cache.increment(cache="test_cache", key="mykey", amount=10)
+#cache.increment(cache="test_cache", key="mykey", amount=10)
 
 
